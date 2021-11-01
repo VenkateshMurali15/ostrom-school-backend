059ccc2a803cbf307d4d3017e26b5a995d1dae86
VenkateshMurali15 <venkateshmurali0@gmail.com>
2021-11-01 13:59:58 +0530

    Change Db

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+    await mongoose.connect(process.env.DB_LIVE_URL, { useNewUrlParser: true });
     console.log("Database Connected");
   } catch (error) {
     console.log("Database Connectivity Error", error);
