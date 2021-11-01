cc06a04b269b350066ff80e75dbda0d664cad8fc
VenkateshMurali15 <venkateshmurali0@gmail.com>
2021-11-01 14:31:48 +0530

    Add Db URl

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.DB_LIVE_URL, { useNewUrlParser: true });
+    await mongoose.connect(
+      "mongodb+srv://ostrom:ostrom123@cluster0.uag2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      { useNewUrlParser: true }
+    );
     console.log("Database Connected");
   } catch (error) {
     console.log("Database Connectivity Error", error);
